b4fde208784a7ecec3bdc0c74325a86237ce03a5
jbarlas <77653620+jbarlas@users.noreply.github.com>
2023-01-20 18:22:44 -0500

    merged from other repo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./App.css";
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 import { intro } from "./assets/content/intro";
-import { sketches } from './assets/content/sketches'
-import { lofi } from "./assets/content/lofi"; 
+import { sketches } from "./assets/content/sketches";
+import { lofi } from "./assets/content/lofi";
 import { crit1, crit2, crit3, crit4, crit5 } from "./assets/content/crit";
-import { hifi } from "./assets/content/hifi"; 
-import { taskQuestions, testingVideos, analysis } from "./assets/content/testing";
+import { hifi } from "./assets/content/hifi";
+import {
+  taskQuestions,
+  testingVideos,
+  analysis,
+} from "./assets/content/testing";
 import ImageCaption from "./components/ImageCaption";
 import ButtonToggle from "./components/ImageToggle";
 import VideoToggle from "./components/VideoToggle";
 import DividedText from "./components/DividedText";
-import { nextSteps } from "./assets/content/capstone";
+import { deployment, development, nextSteps } from "./assets/content/capstone";
 
 function App() {
   return (
@@ -61,43 +65,152 @@ function App() {
       </div>
       <div className="content">
         <div id="intro">
-          <ImageCaption title={intro.title} content={intro.content} img={intro.img}/>
+          <ImageCaption
+            title={intro.title}
+            content={intro.content}
+            img={intro.img}
+          />
         </div>
         <div id="sketches">
-          <ButtonToggle options={sketches.options} title={sketches.title} subtitle={sketches.subtitle}/>
+          <ButtonToggle
+            options={sketches.options}
+            title={sketches.title}
+            subtitle={sketches.subtitle}
+          />
         </div>
         <div id="lo-fi">
-        <ButtonToggle options={lofi.options} title={lofi.title} subtitle={lofi.subtitle}/>
+          <ButtonToggle
+            options={lofi.options}
+            title={lofi.title}
+            subtitle={lofi.subtitle}
+          />
         </div>
         <div id="crit">
-        <div className="section-title">Group Critique</div>
-          <div className="section-subtitle">The HiFi prototype shown above is after we implemented changes from crit feedback, so to show our iteration, we’ve included updated versions of our low fidelity prototypes below (and when not applicable, we have our HiFi). In the LoFi Prototype section above, we have the first draft before feedback.</div>
-        <ImageCaption img={crit1.img} leftSubtitle={crit1.leftSubtitle} leftText={crit1.leftText}/>
-        <ImageCaption img={crit2.img} leftSubtitle={crit2.leftSubtitle} leftText={crit2.leftText}/>
-        <ImageCaption img={crit3.img} leftSubtitle={crit3.leftSubtitle} leftText={crit3.leftText}/>
-        <ImageCaption img={crit4.img} leftSubtitle={crit4.leftSubtitle} leftText={crit4.leftText}/>
-        <ImageCaption img={crit5.img} leftSubtitle={crit5.leftSubtitle} leftText={crit5.leftText}/>
+          <div className="section-title">Group Critique</div>
+          <div className="section-subtitle">
+            The HiFi prototype shown above is after we implemented changes from
+            crit feedback, so to show our iteration, we’ve included updated
+            versions of our low fidelity prototypes below (and when not
+            applicable, we have our HiFi). In the LoFi Prototype section above,
+            we have the first draft before feedback.
+          </div>
+          <ImageCaption
+            img={crit1.img}
+            leftSubtitle={crit1.leftSubtitle}
+            leftText={crit1.leftText}
+          />
+          <ImageCaption
+            img={crit2.img}
+            leftSubtitle={crit2.leftSubtitle}
+            leftText={crit2.leftText}
+          />
+          <ImageCaption
+            img={crit3.img}
+            leftSubtitle={crit3.leftSubtitle}
+            leftText={crit3.leftText}
+          />
+          <ImageCaption
+            img={crit4.img}
+            leftSubtitle={crit4.leftSubtitle}
+            leftText={crit4.leftText}
+          />
+          <ImageCaption
+            img={crit5.img}
+            leftSubtitle={crit5.leftSubtitle}
+            leftText={crit5.leftText}
+          />
         </div>
         <div id="hi-fi">
-        <VideoToggle options={hifi.options} title={hifi.title} subtitle={hifi.subtitle}/>
+          <VideoToggle
+            options={hifi.options}
+            title={hifi.title}
+            subtitle={hifi.subtitle}
+          />
+        </div>
+        <div>
+          <div className="section-subtitle">Figma Prototype</div>
+          <p className="section-text">
+            Below is the Figma file we created to prototype the design
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <iframe
+              title="figma"
+              style={{
+                border: "1px solid rgba(0, 0, 0, 0.1)",
+              }}
+              width="800"
+              height="450"
+              src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FzsHGHe3mr1Shfuf6N6pAhY%2FIterative-Design%3Fnode-id%3D0%253A1%26t%3DDXNbrXtjQILmafhD-1"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
         <div id="testing">
           <div className="section-title">User testing</div>
-          <div className="section-subtitle">Our interface’s intended functionality is first and foremost to allow our users to match and message with others as well as unmatch. Our testing instructions were as follows: "Imagine you’re a woman seeking both men and women on a brand new dating app. Create a profile and try to match with both users that are available to match with. Think aloud as you go through the app, especially when you find something confusing."</div>
-          <DividedText content={taskQuestions.content}/>
-          <br></br><br></br>
-          <div className="section-subtitle">Most of the results were in line with our expectations. All subtasks were successfully done, but there were some confusions. Click the buttons to view each user’s videos.</div>
-          <VideoToggle options={testingVideos.user1} title={testingVideos.user2} subtitle={testingVideos.user3}/>
-          <DividedText content={analysis.content}/>
+          <div className="section-subtitle">
+            Our interface’s intended functionality is first and foremost to
+            allow our users to match and message with others as well as unmatch.
+            Our testing instructions were as follows: "Imagine you’re a woman
+            seeking both men and women on a brand new dating app. Create a
+            profile and try to match with both users that are available to match
+            with. Think aloud as you go through the app, especially when you
+            find something confusing."
+          </div>
+          <DividedText content={taskQuestions.content} />
+          <br></br>
+          <br></br>
+          <div className="section-subtitle">
+            Most of the results were in line with our expectations. All subtasks
+            were successfully done, but there were some confusions. Click the
+            buttons to view each user’s videos.
+          </div>
+          <VideoToggle
+            options={testingVideos.user1}
+            title={testingVideos.user2}
+            subtitle={testingVideos.user3}
+          />
+          <DividedText content={analysis.content} />
         </div>
         <div id="capstone">
-        <div className="section-title">Senior Capstone</div>
-        <div className="section-subtitle">Introduction</div>
-        <p className="section-text">For our capstone, we chose to build upon our iterative design project from CSCI 1300. The course project was our implementation of the iterative design process, in which we iterate on designs for the UI of our app starting from sketches and moving to low fidelity wireframes and eventually high fidelity wireframes. To go from stage to stage, we reviewed and responded to critiques and user testing. In order to substantially develop upon the course assignment, we decided to build out our figma designs into an actual deployed mobile app. We utilized techniques learned in the course, like responsive design, that went beyond our coursework figma specifications. In addition, we decided to use react-native to learn and implement a different framework [is that the right word] not taught in class that has device specific APIs to make the user experience more realistic and substantive.</p>
-        
-        <div className="section-subtitle">Next Steps</div>
-        <p className="section-text">We have three main ideas of how to build out our capstone further:</p>
-        <DividedText content={nextSteps.content} trio={true} />
+          <div className="section-title">Senior Capstone</div>
+          <div className="section-subtitle">Introduction</div>
+          <p className="section-text">
+            For our capstone, we chose to build upon our iterative design
+            project from CSCI 1300. The course project was our implementation of
+            the iterative design process, in which we iterate on designs for the
+            UI of our app starting from sketches and moving to low fidelity
+            wireframes and eventually high fidelity wireframes. To go from stage
+            to stage, we reviewed and responded to critiques and user testing.
+            In order to substantially develop upon the course assignment, we
+            decided to build out our figma designs into an actual deployed
+            mobile app. We utilized techniques learned in the course, like
+            responsive design, that went beyond our coursework figma
+            specifications. In addition, we decided to use react-native to learn
+            and implement a different framework [is that the right word] not
+            taught in class that has device specific APIs to make the user
+            experience more realistic and substantive.
+          </p>
+          <div className="section-subtitle">Development Process</div>
+          <DividedText content={development.content}></DividedText>
+          <div className="section-subtitle">App Deployment</div>
+          <ImageCaption
+            img={deployment.img}
+            leftSubtitle={deployment.leftSubtitle}
+            leftText={deployment.leftText}
+          />
+          <div className="section-subtitle">Next Steps</div>
+          <p className="section-text">
+            We have three main ideas of how to build out our capstone further:
+          </p>
+          <DividedText content={nextSteps.content} trio={true} />
         </div>
       </div>
     </div>
--- src/assets/content/capstone.jsx
+++ src/assets/content/capstone.jsx
@@ -1,6 +1,8 @@
-export const testing = "Now that there is an actual app built out with device specific interactions and slightly different UI, further user testing likely will produce different critiques than in our previous user testing trials with just the Figma. We then can further iterate on our design to make a more user friendly and effective app."
-export const deploy = "When fully deployed on the expo store, specific devices will block the download of the app in Expo Go through a QR code. Android fully allows these types of actions and the app is therefore fully public on Android, but iOS does not. For our capstone, we decided to not further pursue deployment to iOS in addition to Android because Apple is much stricter on apps only being published through their App Store, which costs $99. However, if we want the app to be accessible to more users, we’d want it to be downloadable by both types of mobile phones."
-export const backend = "To make our project a more fully developed app, it needs both a frontend and a backend. Without backend services, everything in the UI is static without a true recommendation algorithm or real users. This would also mean that we build out the profile creation flow detailed in our Figma, because accounts can actually be created and stored. We would build out messaging and profile reaction functionality so the app can become usable for its original purpose."
+import qr from "../images/delight_qr.png"
+
+const testing = "Now that there is an actual app built out with device specific interactions and slightly different UI, further user testing likely will produce different critiques than in our previous user testing trials with just the Figma. We then can further iterate on our design to make a more user friendly and effective app."
+const deploy = "When fully deployed on the expo store, specific devices will block the download of the app in Expo Go through a QR code. Android fully allows these types of actions and the app is therefore fully public on Android, but iOS does not. For our capstone, we decided to not further pursue deployment to iOS in addition to Android because Apple is much stricter on apps only being published through their App Store, which costs $99. However, if we want the app to be accessible to more users, we’d want it to be downloadable by both types of mobile phones."
+const backend = "To make our project a more fully developed app, it needs both a frontend and a backend. Without backend services, everything in the UI is static without a true recommendation algorithm or real users. This would also mean that we build out the profile creation flow detailed in our Figma, because accounts can actually be created and stored. We would build out messaging and profile reaction functionality so the app can become usable for its original purpose."
 
 export const nextSteps = {
     content: {
@@ -12,3 +14,22 @@ export const nextSteps = {
       leftText: backend,
     },
 };
+
+export const development = {
+  content: {
+    rightSubtitle: "React Native/Expo",
+    rightText: "We chose to build the app using a React Native framework called Expo, an open-source framework for apps that run natively on Android, iOS, and the web. React Native differs from React.js in that it can interact directly with the device's native APIs, but uses React syntax/structure.",
+    leftSubtitle: "Development Process",
+    leftText: "The app is structured as various 'Views' within different navigation options. The matching page contains a stack of 'Profile' views which are cycled as the user browses through different profiles. Once a user is matched, the navigation options on the bottom tab bar change to display different options for the matched user. The two biggest hurdles to overcome were figuring out how to navigate between these views and ensuring that the design remained responsive between various screen sizes."
+  }
+}
+
+export const deployment = {
+    img: {
+      src: qr,
+      alt: "Delight QR code",
+      id: "delight_qr"
+    },
+    leftSubtitle: "Delight App Deployment",
+    leftText: "This app is deployed on Expo and can be accessed through the Expo Go app using the following login credentials: Username: delight_capstone, Password: delightful. If you already have an Expo account and need access, please reach out to one of us."
+}
