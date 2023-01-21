fb3ec981d07b7cfa008fc73cfe6632d7441211e7
jbarlas <justin_barlas@brown.edu>
2023-01-20 19:56:19 -0500

    names

--- src/assets/content/intro.jsx
+++ src/assets/content/intro.jsx
@@ -2,7 +2,7 @@ import delight from '../images/delight_logo.png'
 
 export const intro = {
   title: "Introduction",
-  content: "Hello! Below you'll find an interactive interface made for Delight. Our product is a mobile dating app for more serious relationships, so you only talk to one person at a time. This website will walk you through our full process of mocking up a solution for this concept. Enjoy!",
+  content: "Hello! Below you'll find an interactive interface made for Delight made by Justin Barlas, Austin DeStefanis, and Clara Guo. Our product is a mobile dating app for more serious relationships, so you only talk to one person at a time. This website will walk you through our full process of mocking up a solution for this concept. Enjoy!",
   img: {
     src: delight,
     alt: "Delight logo",
